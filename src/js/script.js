5632f6e9186f146ef95a432e6d76057c02ec347a
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2021-11-11 14:44:04 +0100

    add functionality to shopCart. Show or hide shop cart

--- src/js/script.js
+++ src/js/script.js
@@ -280,6 +280,33 @@
     }
   }
 
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element) {
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+      thisCart.dom.toggleTrigger.addEventListener('click', () => thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive));
+    }
+  }
+
   const app = {
     initData: function() {
       const thisApp = this;
@@ -292,11 +319,18 @@
         new Product(productData, thisApp.data.products[productData]);
       }
     },
+    initCart: function () {
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
     init: function(){
       const thisApp = this;
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
