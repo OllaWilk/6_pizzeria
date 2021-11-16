df76bf247f1d3e09acceeb667c48e5393ace3e60
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2021-11-16 19:29:04 +0100

    add subpages

--- src/js/script.js
+++ src/js/script.js
@@ -564,6 +564,41 @@
   }
 
   const app = {
+    initPages: function () {
+      const thisApp = this;
+
+      thisApp.pages = document.querySelector(select.containerOf.pages).children;
+      thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+      const idFromHash = window.location.hash.replace('#/', '');
+
+      let pageMatchingHash = thisApp.pages[0].id;
+      for (let page of thisApp.pages) {
+        if (page.id === idFromHash) {
+          pageMatchingHash = page.id;
+          break;
+        }
+      }
+
+      thisApp.activatePage(idFromHash);
+
+
+
+      for (let link of thisApp.navLinks) {
+        link.addEventListener('click', function (event) {
+          const clickedElement = this;
+          event.preventDefault();
+
+          // get page id from href attribute
+          const id = clickedElement.getAttribute('href').replace('#','');
+          //run thisApp.activatePage with that id
+          thisApp.activatePage(id);
+          // change URL hash
+          window.location.hash = '#/' + id;
+        });
+      }
+
+    },
     initData: function() {
       const thisApp = this;
       thisApp.data = {};
@@ -581,6 +616,24 @@
           thisApp.initMenu();
         });
       console.log('thisApp.data', JSON.stringify(thisApp.data));
+    },
+
+    activatePage: function (pageId) {
+      const thisApp = this;
+
+      //add class active to matching pages, remove from non-matching
+      for (let page of thisApp.pages) {
+        page.classList.toggle(classNames.pages.active, page.id === pageId);
+      }
+      //add class active to matching links, remove from non-matching
+      for (let link of thisApp.navLinks) {
+        link.classList.toggle(
+          classNames.nav.active,
+          link.getAttribute('href') === '#' + pageId
+        );
+
+      }
+
     },
     initMenu: function () {
       const thisApp = this;
@@ -600,6 +653,7 @@
     init: function(){
       const thisApp = this;
 
+      thisApp.initPages();
       thisApp.initData();
       thisApp.initCart();
     },
